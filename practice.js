3953d32763f65100139f59e749271e1e048da9e4
Nithin T <89274566+Nithint4034@users.noreply.github.com>
2023-07-14 05:16:10 +0530

    comments

---
+++ practice.js
@@ -0,0 +1,17 @@
+// const express = require('express');
+// const app = express();
+// const bcrypt = require('bcrypt');
+// const bodyParser = require('body-parser');
+// const userModel = require('./model/model');
+// const mongoose = require('mongoose');
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended:true}));
+
+// app.get('/', ()=>{
+//     res.send('Login and Register');
+// })
+
+// app.listen(3000, ()=>{
+//     console.log("Server listening to the port 3000");
+// })
